8f6c037b3bae289ce00f90230c9a459d56f14830
umutbarlak <umtbarlak@gmail.com>
2024-09-30 19:02:08 +0300

    filter side fixed top sticky

--- src/pages/HomePage/LeftSide.jsx
+++ src/pages/HomePage/LeftSide.jsx
@@ -21,36 +21,40 @@ const LeftSide = () => {
   return (
     <aside
       className={`bg-gray-300 p-3 rounded-md transition-all duration-500 overflow-scroll scroll-p-2 ${
-        show ? "max-md:h-fit" : "max-md:h-14 "
+        show ? "max-md:h-[500px]" : "max-md:h-14 "
       } ${show ? "md:w-64" : "md:w-14 "}`}
     >
       <>
-        <div className="flex gap-2">
-          <button
-            className="text-3xl text-white"
-            onClick={() => setShow(!show)}
-          >
-            <CgMenuRound />
-          </button>
-          <h3
-            className={`text-2xl tracking-widest text-sky-800  ${
-              !show && "md:hidden"
-            }`}
-          >
-            Filter
-          </h3>
-        </div>
-        <div
-          className={`flex flex-col justify-between h-full transition-opacity mt-3 duration-700  ${
-            !show && "hidden"
-          }  `}
-        >
+        <div className="sticky top-0 left-0 right-0 bg-gray-300">
+          <div className="flex gap-2">
+            <button
+              className="text-3xl text-white"
+              onClick={() => setShow(!show)}
+            >
+              <CgMenuRound />
+            </button>
+            <h3
+              className={`text-2xl tracking-widest text-sky-800  ${
+                !show && "md:hidden"
+              }`}
+            >
+              Filter
+            </h3>
+          </div>
           <button
             onClick={() => dispatch(getAllCategories())}
-            className={`p-1 rounded-lg capitalize text-lg tracking-wide cursor-pointer bg-gray-100 text-sky-800 hover:shadow-md`}
+            className={`p-1 rounded-lg capitalize text-lg tracking-wide cursor-pointer bg-gray-100 text-sky-800 hover:shadow-md w-full mt-4 ${
+              !show && "hidden"
+            }`}
           >
             Tümü
           </button>
+        </div>
+        <div
+          className={`flex flex-col gap-3 h-full transition-opacity mt-3 duration-700  ${
+            !show && "hidden"
+          }  `}
+        >
           {/* <div>
             <h3 className={`text-2xl tracking-widest text-sky-800  `}>
               Category
